beca7efe6fdd34fd99e48db0ac3734af586e51fb
Anagha Subedi <167497533+anaghasubedi@users.noreply.github.com>
2026-05-24 13:02:10 +0545

    Theme edits

--- frontend/roomie/lib/themes/colors.dart
+++ frontend/roomie/lib/themes/colors.dart
@@ -7,7 +7,7 @@ class RoomieColors {
   static const Color primarySoft = Color(0xFF5A4A9B);
   static const Color border = Color(0xFFC4BAF0);
   static const Color bg = Color(0xFFF7F5FF);
-  static const Color text = Color(0xFF2D1F6E);
+  static const Color text = Color(0x80000000);
   static const Color hint = Color(0xFFB0A3D8);
   static const Color orText = Color(0xFF9E8FCC);
   static const Color buttonText = Color(0xFFF0EDFF);
