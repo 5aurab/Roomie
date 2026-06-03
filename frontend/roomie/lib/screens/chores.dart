c90e447bed4b602484f0fe72c73cfd27909ec62b
5aurab <sneupane.0145@gmail.com>
2026-06-03 10:32:03 -0400

    fix: sync with upstream main

--- frontend/roomie/lib/screens/chores.dart
+++
@@ -1,9 +0,0 @@
-﻿import 'package:flutter/material.dart';
-
-class ChoresScreen extends StatelessWidget {
-  const ChoresScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('ChoresScreen'));
-  }
-}
--- frontend/roomie/lib/screens/surprise.dart
+++
@@ -1,9 +0,0 @@
-﻿import 'package:flutter/material.dart';
-
-class SurpriseScreen extends StatelessWidget {
-  const SurpriseScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('SurpriseScreen'));
-  }
-}
